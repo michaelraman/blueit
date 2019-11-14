1eae731b1cdf9c9bce713f9607e25a7efa01baf7
Lily Do <lilydo@haido.local>
2019-11-14 11:05:44 -0500

    variable name change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function addItem(){
     var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("topic");
+    var topic = document.getElementById("topic");
     var li = document.createElement("li");
     li.setAttribute('id',topic.value);
     li.innerHTML = '<a href=\"messages.html?topic=' + topic.value + '\">topic.value</a>'  // '<a href="#hOME">HTML</a>'
