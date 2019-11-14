32260aad047cd1f794ddecf6a56872ba54f232d3
Lily Do <lilydo@haido.local>
2019-11-14 10:59:35 -0500

    created hyperlinks

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function addItem(){
     var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
+    var candidate = document.getElementById("topic");
     var li = document.createElement("li");
-    li.setAttribute('id',candidate.value);
-    li.appendChild(document.createTextNode(candidate.value));
+    li.setAttribute('id',topic.value);
+    li.innerHTML = '<a href=\"messages.html?topic=' + topic.value + '\">topic.value</a>'  // '<a href="#hOME">HTML</a>'
+    li.setAttribute('href', topic.value + ".html")
+    li.appendChild(document.createTextNode(topic.value));
     ul.appendChild(li);
 }
