2f832bf2a5ad426d1453df618af09683374263dd
Lily Do <lilydo@haido.local>
2019-11-14 10:17:09 -0500

    making a dynamic list

---
+++ script.js
@@ -0,0 +1,8 @@
+function addItem(){
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("candidate");
+    var li = document.createElement("li");
+    li.setAttribute('id',candidate.value);
+    li.appendChild(document.createTextNode(candidate.value));
+    ul.appendChild(li);
+}
