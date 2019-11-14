c5e6157bbeaf27cea6a435be5e9747403f1730b7
Mithil Verma <32003804+MithilV@users.noreply.github.com>
2019-11-14 10:32:54 -0500

    Add files via upload

---
+++ firebaseIndex.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+// Your web app's Firebase configuration
+    apiKey: "AIzaSyDQ7FMpVsxFPQrIVAb-pR4Fq7AEEWqNUag",
+    authDomain: "redditforum-9aaad.firebaseapp.com",
+    databaseURL: "https://redditforum-9aaad.firebaseio.com",
+    projectId: "redditforum-9aaad",
+    storageBucket: "redditforum-9aaad.appspot.com",
+    messagingSenderId: "889538924197",
+    appId: "1:889538924197:web:1734e4b342b7967cd4c74c",
+    measurementId: "G-NE9BL2EC80"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
