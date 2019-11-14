9d6cddc7d5e9d85ecf6441b3cf1143c1847cbfad
Michael Raman <michael.raman@me.com>
2019-11-14 09:56:41 -0500

    Firebase boilerplate

---
+++ firebase.js
@@ -0,0 +1,14 @@
+var config = {
+    // apiKey: "AIzaSyCHhX5PmpsmLQhY95HmLnm2VYc0m81mos0",
+    // authDomain: "tyes-web-478b4.firebaseapp.com",
+    // databaseURL: "https://tyes-web-478b4.firebaseio.com", //REPLACE
+    // projectId: "tyes-web-478b4",
+    // storageBucket: "tyes-web-478b4.appspot.com",
+    // messagingSenderId: "496916704132",
+    // appId: "1:496916704132:web:c313edfb1c7b1a41a40bd1"
+
+    // mithil: update w config
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+var db = firebase.database();
