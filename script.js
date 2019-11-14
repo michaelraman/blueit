c53607dd6c071c9c4f39b88dfab798abf9bcbcd6
Lily Do <lilydo@haido.local>
2019-11-14 11:10:09 -0500

    fixed linking looks

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function addItem(){
     var topic = document.getElementById("topic");
     var li = document.createElement("li");
     li.setAttribute('id',topic.value);
-    li.innerHTML = '<a href=\"messages.html?topic=' + topic.value + '\">topic.value</a>'  // '<a href="#hOME">HTML</a>'
+    li.innerHTML = '<a href=\"messages.html?topic=' + topic.value + '\">' + topic.value + '</a>'  // '<a href="#hOME">HTML</a>'
     li.setAttribute('href', topic.value + ".html")
-    li.appendChild(document.createTextNode(topic.value));
+    //li.appendChild(document.createTextNode(topic.value));
     ul.appendChild(li);
 }
 
